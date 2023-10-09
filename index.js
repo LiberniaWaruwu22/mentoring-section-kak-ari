d40c822a7b1b29d658446d880988f54217e720bf
Libernia Waruwu <waruwulibernia@gmal.com>
2023-10-09 12:54:25 +0700

    Update

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const http = require("http");
 const port = process.env.PORT || 3000;
-http
-  .createServer((req,res) =>{
-    res.write("hello world");
-    res.end();
-})
- .listen(port,()=>{
-    console.log(`Server running on localhost:${port}`);
+
+const server = http.createServer((req, res) => {
+  res.write("Hello, world!");
+  res.end();
+});
+
+server.listen(port, "0.0.0.0", () => {
+  console.log(`Server running on localhost:${port}`);
 });
